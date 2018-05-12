354ab1fbe2bfc8304929662c275f0ed2699c7df6
Binnur Al-Kazily <binnur@me.com>
2018-05-12 14:12:51 -0700

    Added bling control

--- src/main/java/com/spartronics4915/atlas/commands/ActivateLauncherCommandGroup.java
+++ src/main/java/com/spartronics4915/atlas/commands/ActivateLauncherCommandGroup.java
@@ -1,14 +1,17 @@
 package com.spartronics4915.atlas.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-
+import com.spartronics4915.atlas.commands.SetBlingStateCommand;
+import com.spartronics4915.atlas.subsystems.LED;
+import com.spartronics4915.atlas.subsystems.LED.BlingState;
 /**
  * CommandGroup to actuate wind launcher
  */
 public class ActivateLauncherCommandGroup extends CommandGroup
 {
-    public ActivateLauncherCommandGroup()
+    public ActivateLauncherCommandGroup(LED mLed)
     {
+        addSequential(new SetBlingStateCommand(mLed, BlingState.SPARTRONICS_FADE));
         // TODO: ensure harvester is extended
         // addSequential(new ExtendHarvester());
         addSequential(new ActivateLauncher());
