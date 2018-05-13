0677456d7838f85616b80330ed536d5060ec0e73
Binnur Al-Kazily <binnur.gul@gmail.com>
2018-05-12 20:41:17 -0700

    Added OI support for launcher w/ bling (#9)
    * Added ball detection 'todos'
    
    * Added bling control
    
    * Delete autonomous references in OI
    
    * OI controls for launcher

--- src/main/java/com/spartronics4915/atlas/OI.java
+++ src/main/java/com/spartronics4915/atlas/OI.java
@@ -6,8 +6,14 @@ import java.time.Instant;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
+import com.spartronics4915.atlas.commands.*;
+import com.spartronics4915.atlas.subsystems.LED;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import sun.misc.Launcher;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -16,13 +22,31 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class OI
 {
     private Robot mRobot;
+    private LED mLED;
 
+    private static final int kDriveJoystickPort = 1;
+    private static final int kArcadeStickPort = 2;
+
+    // launcher controls TODO: Fix button
+    private static final int kLaunchDriveStickButton = 2;
+    private static final int kLaunchArcadeStickButton = 2;
+    private static final int kWindLauncherDriveStickButton = 3;
+    private static final int kWindLauncherArcadeStickButton = 3;
+
+    // harvester controls TODO: Fix button
+    private static final int kHarvesterExtendDriveStickButton = 4;
+    private static final int kHarvesterExtendArcadeStickButton = 4;
+    private static final int kHarvesterRetractDriveStickButton = 5;
+    private static final int kHarvesterRetractArcadeStickButton = 5;
+    private static final int kHarvesterWheelsForwardDriveStickButton = 6;
+    private static final int kHarvest6rWheelsReverseDriveStickButton = 7;
+
+    // add harvester buttons -- extend & retract
+    // add harvester wheel buttons -- trigger style forward & reverse
+    
     public OI(Robot robot)
     {
         mRobot = robot;
-
-        initAutoOI();
-        initDrivetrainOI();
 
         // TODO: Reimplement in Gradle
         // Version string and related information
@@ -48,27 +72,26 @@ public class OI
             Logger.error("Build version not found!");
             Logger.logThrowableCrash(e);
         }
-    }
 
-    private void initAutoOI()
-    {
-        // Add autonomous options to SmartDashboard here
-        // FIXME
-    }
+        // initialize joysticks
+        Joystick driveStick = new Joystick(kDriveJoystickPort);
+        Joystick arcadeStick = new Joystick(kArcadeStickPort);
 
-    // getAutoCommand should happen during autonomous init (so long after robotInit).
-    // Timing is important here.
-    public Command getAutoCommand()
-    {
-        return new Command()
-        {
+        // get instance to LED system
+        mLED = LED.getInstance();
 
-            @Override
-            protected boolean isFinished()
-            {
-                return false;
-            }
-        }; // FIXME
+        initDrivetrainOI();
+
+        // initialize launchers buttons
+		JoystickButton launchCommandGroupButtonOnDriveStick = new JoystickButton(driveStick, kLaunchDriveStickButton);
+        launchCommandGroupButtonOnDriveStick.whenPressed(new ActivateLauncherCommandGroup(mLED));
+        JoystickButton launchCommandGroupButtonOnArcadeStick = new JoystickButton(arcadeStick, kLaunchArcadeStickButton);
+		launchCommandGroupButtonOnArcadeStick.whenPressed(new ActivateLauncherCommandGroup(mLED));
+
+        JoystickButton windCommandGroupButtonOnDriveStick = new JoystickButton(driveStick, kWindLauncherDriveStickButton);
+        windCommandGroupButtonOnDriveStick.whenPressed(new WindLauncherCommandGroup(mLED));
+        JoystickButton windCommandGroupButtonOnArcadeStick = new JoystickButton(arcadeStick, kLaunchArcadeStickButton);
+        windCommandGroupButtonOnArcadeStick.whenPressed(new WindLauncherCommandGroup(mLED));
     }
 
     private void initDrivetrainOI()
--- src/main/java/com/spartronics4915/atlas/commands/WindLauncherCommandGroup.java
+++ src/main/java/com/spartronics4915/atlas/commands/WindLauncherCommandGroup.java
@@ -1,16 +1,18 @@
 package com.spartronics4915.atlas.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import com.spartronics4915.atlas.subsystems.LED;
+import com.spartronics4915.atlas.subsystems.LED.BlingState;
 
 /**
  * CommandGroup to actuate wind launcher
  */
 public class WindLauncherCommandGroup extends CommandGroup
 {
-    public WindLauncherCommandGroup()
+    public WindLauncherCommandGroup(LED mLed)
     {
         // TODO: ensure harvester is extended
         // addSequential(new ExtendHarvester());
-        addSequential(new WindLauncher());
+        addSequential(new WindLauncher(mLed, BlingState.FAST_FLASHING));
     }
 }
